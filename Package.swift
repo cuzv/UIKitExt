b1b3242c855f2e60993195583352b257d6bad149
Shaw <cuzval@gmail.com>
2020-08-14 17:08:45 +0800

    Add image render func.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "UIExt",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
