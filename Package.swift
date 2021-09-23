f0a866f4667328f9a327198ab044ca1da8dfea3f
Shaw <cuzval@gmail.com>
2021-09-23 10:36:13 +0800

    Workaround for build with Combine

--- Package.swift
+++ Package.swift
@@ -6,7 +6,12 @@ import PackageDescription
 let package = Package(
   name: "UIKitExt",
   platforms: [
-    .iOS(.v10),
+    // https://developer.apple.com/documentation/xcode-release-notes/xcode-13-release-notes
+    // ```
+    // Swift libraries depending on Combine may fail to build for targets including armv7 and i386 architectures. (82183186, 82189214)
+    // Workaround: Use an updated version of the library that isn’t impacted (if available) or remove armv7 and i386 support (for example, increase the deployment target of the library to iOS 11 or higher).
+    //```
+    .iOS(.v11),
   ],
   products: [
     .library(name: "UIKitExt", targets: ["UIKitExt"]),
