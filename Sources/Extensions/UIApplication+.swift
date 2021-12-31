b39bf61d0bdb0ff42209650e1f55e97d82ed1849
Shaw <cuzval@gmail.com>
2021-12-31 10:42:17 +0800

    open AppStore

--- Sources/Extensions/UIApplication+.swift
+++ Sources/Extensions/UIApplication+.swift
@@ -15,4 +15,26 @@ extension UIApplication {
       print("Failed to delete launch screen cache: \(error)")
     }
   }
+
+  @available(iOS 13.0, tvOS 13.0, macCatalyst 13.0, *)
+  public func openAppStore(appId: String, writeReview: Bool = false) async throws -> Bool {
+    var link = "itms-apps://itunes.apple.com/app/id\(appId)?mt=8"
+    if writeReview {
+      link.append("&action=write-review")
+    }
+
+    guard let url = URL(string: link) else {
+      throw URLError.invalid
+    }
+
+    return await withCheckedContinuation { continuation in
+      open(url, options: [:]) { result in
+        continuation.resume(with: .success(result))
+      }
+    }
+  }
+}
+
+public enum URLError: Error {
+  case invalid
 }
