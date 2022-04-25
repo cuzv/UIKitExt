acc775269d72325c7b22898efbcdfeb604d6a79f
Shaw <cuzval@gmail.com>
2022-04-25 15:25:37 +0800

    Neat

--- Sources/Extensions/UIStackView+.swift
+++ Sources/Extensions/UIStackView+.swift
@@ -18,18 +18,20 @@ extension UIStackView {
 
 extension UIStackView {
   @available(iOS 11.0, tvOS 11.0, macCatalyst 11.0, *)
-  public static func create(
+  public convenience init(
     axis: NSLayoutConstraint.Axis,
     alignment: UIStackView.Alignment = .fill,
     distribution: UIStackView.Distribution = .fill,
     spacing: CGFloat = 0,
-    margins: NSDirectionalEdgeInsets = .init()
-  ) -> UIStackView {
-    UIStackView(
-      axis: axis, alignment: alignment,
-      distribution: distribution, spacing: spacing)
-    .useConstraints()
-    .directionalLayoutMargins(margins)
-    .layoutMarginsRelativeArrangement(true)
+    layoutMargins: NSDirectionalEdgeInsets = .init()
+  ) {
+    self.init()
+    self.axis = axis
+    self.alignment = alignment
+    self.distribution = distribution
+    self.spacing = spacing
+    self.translatesAutoresizingMaskIntoConstraints = false
+    self.directionalLayoutMargins = layoutMargins
+    self.isLayoutMarginsRelativeArrangement = layoutMargins != .zero
   }
 }
